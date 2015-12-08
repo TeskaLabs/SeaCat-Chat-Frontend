0871848c84c45c51c0f9d0f9d9a1fc70bb39cd75
Miloslav Pavelka <pavelkamiloslav@gmail.com>
2015-12-08 19:08:17 +0100

    Reads one CSR from file or standard input. Enhancing usage texts.

--- on_incoming_csr/on_incoming_csr.py
+++ on_incoming_csr/on_incoming_csr.py
@@ -1,171 +1,120 @@
-import sys, getopt, os, json
+import ConfigParser, argparse, sys, getopt, os, json
 import time, datetime
 import hashlib
 from asn1dertools import extract_csr_info_json
 
-_OPTIONS = {
-	"SECRET" 			: "app-secret-changeme",
-	"MONGODB_URI" 		: None,#"mongodb://localhost:27017/"
-	"MONGODB_DB" 		: "scapdb",
-	"MONGODB_TABLE"		: "CSR",
-	"FRONTEND_REQ_URL" 	: None,# "http://localhost:5000/newcsr"
-	"PATH_CSR_NEW" 		: None,# "./new"
-	"PATH_CSR_WAITING" 	: None,# "./wait"
-}
+
+config = ConfigParser.ConfigParser()
 
 
-
-def read_csr(csr_filename):
-	csr_der 		= open(csr_filename)
-	ret 			= {}
-	ret['filename'] = os.path.basename(csr_filename)
-	ret['csr'] 		= extract_csr_info_json(csr_der.read())
-
-	return ret
-
-
-
-def filename_to_ticket(csr_name, timestamp_expires=None):
+def filename_to_ticket(filename, timestamp_expires=None):
 	if timestamp_expires is None:
 		timestamp_expires = int(time.time())+5*24*60*60 # 5 days
 
 	str_timestamp_expires = str(timestamp_expires)
-	ticket = hashlib.sha224(csr_name+_OPTIONS["SECRET"]+str_timestamp_expires).hexdigest()
+	ticket = hashlib.sha224(os.path.splitext(filename)[0]+config.get("default", "secret")+str_timestamp_expires).hexdigest()
 	return ticket + "_" + str_timestamp_expires
 
 
 
-def store_csr_mongo(csr_dict):
+def store_csr_mongo(csr_dict, mongodb_uri, mongodb_db, mongodb_table):
 	import pymongo
-	Client 	= pymongo.MongoClient(_OPTIONS["MONGODB_URI"])
-	DB 		= Client[_OPTIONS["MONGODB_DB"]]
-	print "Storing to {} in database {} at {}".format(
-		_OPTIONS["MONGODB_TABLE"],
-		_OPTIONS["MONGODB_DB"],
-		_OPTIONS["MONGODB_URI"])
+	Client 	= pymongo.MongoClient(mongodb_uri)
+	DB 		= Client[mongodb_db]
 	try:
-		DB[_OPTIONS["MONGODB_TABLE"]].insert(csr_dict.copy())
-		print "DONE"
+		DB[mongodb_table].insert(csr_dict.copy())
+		print "Stored to MongoDB..."
 	except Exception as e: print e
 
 
 
-def store_csr_frontend(req_dict, method='PUT'):
+def store_csr_frontend(csr_dict, req_url, method='PUT'):
 	import urllib2
 	opener = urllib2.build_opener(urllib2.HTTPHandler)
-	print "{} {} {}".format(str(req_dict), method, _OPTIONS["FRONTEND_REQ_URL"])
 	try:
 		request = urllib2.Request(
-			url=_OPTIONS["FRONTEND_REQ_URL"],
-			data=json.dumps(req_dict))
+			url=req_url,
+			data=json.dumps(csr_dict))
 		request.add_header('Content-Type', 'application/json')
 		request.get_method = lambda: method
 	
 		url = opener.open(request)
-		print "DONE"
+		print "Sent to frontend..."
 	except Exception as e: print e
 
 
 
-def process_csr(csr_filename):
-	# Create ticket
-	filename, ext 	= os.path.splitext(csr_filename)
-	ticket = filename_to_ticket(filename)
-
-	# Rename to ticket name
-	abspath_csr_filename 	= os.path.abspath(csr_filename)
-	abspath_csr_dirname 	= os.path.dirname(abspath_csr_filename)
-	new_csr_filename 			= os.path.join(abspath_csr_dirname, ticket+ext)
-	os.rename(abspath_csr_filename, new_csr_filename)
-
-	# Read csr
-	csr_dict = read_csr(new_csr_filename)
+def process_csr(file_csr, csr_filename):
+	# Assemble data
+	csr_dict = {
+		"filename"	: csr_filename,
+		"ticket"	: filename_to_ticket(csr_filename),
+		"csr"		: extract_csr_info_json(file_csr.read())
+	}
+	print csr_dict
 	
 	# Store to database
-	if _OPTIONS["MONGODB_URI"] is not None:
-		store_csr_mongo(csr_dict)
+	if config.has_section("mongodb"):
+		store_csr_mongo(csr_dict,
+			mongodb_uri=config.get("mongodb", "uri"),
+			mongodb_db=config.get("mongodb", "db"),
+			mongodb_table=config.get("mongodb", "table"))
 	# Send ticket to frontend
-	if _OPTIONS["FRONTEND_REQ_URL"] is not None:
-		store_csr_frontend(csr_dict)
-
-	print "finished processing {} -> {}".format(csr_filename, new_csr_filename)
-
-
-
-def process_new_csrs():
-	for f in os.listdir(_OPTIONS["PATH_CSR_NEW"]):
-		if not f.endswith(".csr"):
-			continue
-
-		# Move to folder _OPTIONS["PATH_CSR_WAITING"]
-		# This is done first so that there are no time delays
-		# and so that the script doesn't process previously processed csrs
-		if not os.path.isfile(os.path.join(_OPTIONS["PATH_CSR_NEW"], f)):
-			continue
-		os.rename(os.path.join(_OPTIONS["PATH_CSR_NEW"], f), os.path.join(_OPTIONS["PATH_CSR_WAITING"], f))
-
-		# Process the csr
-		process_csr(os.path.join(_OPTIONS["PATH_CSR_WAITING"], f))
+	if config.has_option("frontend", "url"):
+		store_csr_frontend(csr_dict, config.get("frontend", "url"))
 
 
 
 def main(argv):
-	try:
-		opts, args = getopt.getopt(argv,
-			"hn:w:m:d:t:f:",
-			[
-				"help",
-				"new-csrs=",
-				"waiting-csrs=",
-				"mongodb-uri=",
-				"mongodb-name=",
-				"mongodb-table=",
-				"frontend-url="
-			])
-	except getopt.GetoptError:
-		usage() 
+	global config
+
+	parser = argparse.ArgumentParser(epilog=get_usage_epilog(), formatter_class=argparse.RawTextHelpFormatter)
+	parser.add_argument("-c", "--config", help="path to configuration file", required=True)
+	parser.add_argument("-f", "--filename", dest="csr_filename", help="CSR file name (required if csr argument is '-')")
+	parser.add_argument("csr", help="CSR file path (if set to '-' CSR is read from stdin)")
+	
+	args = parser.parse_args()
+	
+	config.read(args.config)
+
+	if not config.has_option("default", "secret"):
+		print "Error: You must provide secret key in config file."
+		parser.print_help()
 		sys.exit()
+	if config.has_section("mongodb"):
+		if not config.has_option("mongodb", "uri") \
+			or not config.has_option("mongodb", "db") \
+			or not config.has_option("mongodb", "table"):
+			print "Error: You must specify URI, DB and table name in config file."
+			parser.print_help()
+			sys.exit(1)
 
-	global _OPTIONS
+	if args.csr == "-":
+		if args.csr_filename is None:
+			print "Error: You must specify CSR file name when providing CSR in stdin."
+			parser.print_help()
+			sys.exit(1)
+		# Read CSR from stdin
+		file_csr = sys.stdin
+	else:
+		args.csr_filename 	= os.path.basename(args.csr)
+		file_csr 			= open(args.csr)
 
-	for opt, arg in opts:
-		if opt in ("-h", "--help"):
-			usage()
-			sys.exit()
+	process_csr(file_csr, args.csr_filename)
 
-		elif opt in ("-n", "--new-csrs"):
-			_OPTIONS["PATH_CSR_NEW"] = arg
 
-		elif opt in ("-w", "--waiting-csrs"):
-			_OPTIONS["PATH_CSR_WAITING"] = arg
-
-		elif opt in ("-m", "--mongodb-uri"):
-			_OPTIONS["MONGODB_URI"] = arg
-
-		elif opt in ("-d", "--mongodb-name"):
-			_OPTIONS["MONGODB_DB"] = arg
-
-		elif opt in ("-t", "--mongodb-table"):
-			_OPTIONS["MONGODB_TABLE"] = arg
-
-		elif opt in ("-f", "--frontend-url"):
-			_OPTIONS["FRONTEND_REQ_URL"] = arg
-
-	if _OPTIONS["PATH_CSR_NEW"] is None:
-		print "Path to new CSRs not specified."
-		usage()
-		sys.exit()
-
-	if _OPTIONS["PATH_CSR_WAITING"] is None:
-		print "Path to waiting CSRs not specified."
-		usage()
-		sys.exit()
-
-	process_new_csrs()
-
-def usage():
-	print "on_incoming_csr.py -n <newcsrsfolder> -w <waitingcsrsfolder> -m <mongodburi> -d <mongodbname> -t <mongodbtable> -f <frontendurl>"
+def get_usage_epilog():
+	return """
+config file:
+  [default]
+    secert=		secret key used to hashing ticket
+  [mongodb]
+    uri=		mongoDB URI where CSR will be saved
+    db=			database name in MongoDB
+    table=		table name in MongoDB
+  [frontend]
+    url=		frontend URL where CSR will be sent
+"""
 
 if __name__ == "__main__":
 	main(sys.argv[1:])
-
